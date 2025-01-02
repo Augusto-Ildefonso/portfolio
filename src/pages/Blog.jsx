38dfebe019a6aca415e84f67e3a8adb71d505af6
Augusto <afildefonso16@gmail.com>
2025-01-02 16:11:57 -0300

    responsividade para a página do blog

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -3,24 +3,68 @@ import NavBar from "../components/NavBar/NavBar";
 import BlogCard from "../components/BlogCard/BlogCard";
 import '../styles/Blog.css'
 import {Helmet} from "react-helmet";
+import {useMediaQuery} from "react-responsive";
 
 export default function Blog (){
+    const isBigScreen = useMediaQuery({query: 'only screen and (min-width: 1500px)'});
+    const isMediumScreen = useMediaQuery({query: 'only screen and (min-width: 600px) and (max-width: 1499px)'});
+    const isSmallScreen = useMediaQuery({query: 'only screen and (max-width: 599px)'});
+
     return (
         <div className="BlogPage">
             <Helmet>
                 <title>Blog | Augusto Ildefonso</title>
             </Helmet>
-            <div className='ContentArea'>
 
-                <NavBar currentPage='blog'/>
+            {isBigScreen && (
+                <div className='ContentAreaBlogPage'>
 
-                <h1 className='PageTitle'>Blog</h1>
+                    <NavBar currentPage='blog'/>
 
-                <div className='BlogCardsArea'>
-                    <h2>No posts until the moment...</h2>
+                    <h1 className='PageTitleBlog'>Blog</h1>
+
+                    <div className='BlogCardsArea'>
+                        <h2 className='NoPostsTitle'>No posts until the moment...</h2>
+                    </div>
+
                 </div>
+            )}
 
-            </div>
+            {isMediumScreen && (
+                <div className='ContentAreaBlogPage'>
+
+                    <NavBar currentPage='blog'/>
+
+                    <h1 className='PageTitleBlog'>Blog</h1>
+
+                    <div className='BlogCardsArea'>
+                        <h2 className='NoPostsTitle'>No posts until the moment...</h2>
+                    </div>
+
+                </div>
+            )}
+
+            {isSmallScreen && (
+                <React.Fragment>
+                    <Helmet>
+                        <meta name="theme-color" content="black"/>
+                    </Helmet>
+
+                    <div className='ContentAreaBlogPage'>
+
+                        <NavBar currentPage='blog'/>
+
+                        <h1 className='PageTitleBlog'>Blog</h1>
+
+                        <div className='BlogCardsArea'>
+                            <h2 className='NoPostsTitle'>No posts until the moment...</h2>
+                        </div>
+
+                    </div>
+                </React.Fragment>
+            )}
+
+
         </div>
     );
 }
